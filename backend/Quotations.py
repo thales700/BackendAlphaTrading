f0ffb735a23d3655f7d1f9452a2a0c773bcb74c9
thalesuds700 <thales.uds@gmail.com>
2025-11-03 20:40:25 -0300

    Setting Logging and Data Validation

--- backend/Quotations.py
+++ backend/Quotations.py
@@ -1,7 +1,11 @@
+import datetime
 import yfinance as yf
 import pandas as pd
 from Symbols import Symbols
 from typing import Union, Optional
+import logging
+
+logger = logging.getLogger(__name__)
 
 class Quotations:
     def __init__(self) -> None:
@@ -9,14 +13,31 @@ class Quotations:
     
     def _VerifySymbol(self, symbol: str) -> bool:
         return symbol in self.symbolsValues
-    
-    def Get(self, symbol: str, start_date: str, end_date: str) -> Union[pd.DataFrame, None]:
-        if not self._VerifySymbol(symbol):
-            return None
-        else:
-            ticker = yf.Ticker(symbol)
-            data = ticker.history(start=start_date, end=end_date)
-            df = pd.DataFrame(data)
-            return df
 
-print(Quotations().Get(Symbols.AAPL.value, "2023-01-01", "2025-01-31"))
+    def _VerifyDate(self, date: str) -> bool:
+        try:
+            datetime.date.fromisoformat(date)
+            return True
+        except ValueError:
+            logger.error(f"Date format error: {date} is not in YYYY-MM-DD format.")
+            return False
+        
+    def Get(self, symbol: str, start_date: str, end_date: str) -> Union[pd.DataFrame, str]:
+        try:
+            if not self._VerifySymbol(symbol):
+                return "No such symbol"
+            
+            if not self._VerifyDate(start_date) or not self._VerifyDate(end_date):
+                return "Date format error. Use YYYY-MM-DD."
+
+            else:
+                ticker = yf.Ticker(symbol)
+                data = ticker.history(start=start_date, end=end_date)
+                df = pd.DataFrame(data)
+                logger.info(f"Successfully retrieved data for {symbol} from {start_date} to {end_date}")
+                return df
+            
+        except Exception as e:
+            logger.error(f"Error retrieving data for {symbol} from {start_date} to {end_date}: {e}")
+            return str(e)
+
---
+++ backend/main.py
@@ -0,0 +1,12 @@
+from Quotations import Quotations
+from Symbols import Symbols
+import logging
+
+logger = logging.getLogger(__name__)
+LOG_FORMAT = '%(asctime)s | %(levelname)s | %(filename)s:%(lineno)d | %(message)s'
+logging.basicConfig(
+    level=logging.INFO,
+    format=LOG_FORMAT
+)
+
+print(Quotations().Get(Symbols.AAPL.value, "2023-01-01", "2025-01-31"))
